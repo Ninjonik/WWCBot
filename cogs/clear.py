e2579d3bc17da94a545e8486c5adf3bad83c26ea
NinjonikSVK <petricekzatko@gmail.com>
2023-01-10 18:18:33 +0100

    - fixed critical clear.py bug (it was useable without permissions)

--- cogs/clear.py
+++ cogs/clear.py
@@ -10,9 +10,13 @@ class clear(commands.Cog):
 
     @app_commands.command(name="clear", description="Clears last x messages in a channel")
     async def clear(self, interaction: discord.Interaction, amount: int):
-        if presets.check_perm(interaction.user):
+        member = interaction.user
+        userName = f"{member.name}#{member.discriminator}"
+        if presets.check_perm(userName):
             await interaction.response.send_message(content=f"{amount} messages have been removed.")
             await interaction.channel.purge(limit=amount+1)
+        else:
+            await interaction.response.send_message(content=f"Not enough permissions for this command!", ephemeral=True)
 
 
 async def setup(client: commands.Bot) -> None:

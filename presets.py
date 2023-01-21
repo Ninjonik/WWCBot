ec59d5cd1722aab8a389fcc75b32c9c29dccc6a1
NinjonikSVK <petricekzatko@gmail.com>
2023-01-21 13:01:00 +0100

    - removed monkey from presets.py

--- presets.py
+++ presets.py
@@ -161,43 +161,9 @@ class EntryDialog(discord.ui.View):
                     if user_key.content == key:
                         log(f" User {member.name}#{member.discriminator} "
                             f"has successfully solved the captcha with {tries} tries left.")
-                        embed = discord.Embed(
-                            title=f"Customization - **Get  Roles**",
-                            description="You've successfully completed the verification! "
-                                        "**Now you'll be locked in a General section from which "
-                                        "you can get out by simply chatting with others.** You can "
-                                        "unlock additional channels immediately by clicking on one of the buttons below.",
-                            colour=discord.Colour.blurple()
-                        )
-                        embed.set_thumbnail(url=interaction.guild.icon)
-                        embed.add_field(
-                            name='**War Discussion**',
-                            value="If you came to the server to discuss various historical content or you have "
-                                  "any questions about uniform/badge/... you've found -> it's the place for you.",
-                            inline=False,
-                        )
-                        embed.add_field(
-                            name="**Military Games**",
-                            value='If you came to the server to take part in a discussion or came to find other people '
-                                  'to play various Historical Games that took place in the 20th century, '
-                                  "then it's the place for you!",
-                            inline=True,
-                        )
-                        embed.add_field(
-                            name="**Roblox Access**",
-                            value='Click on this role if you want to stay updated on the brand new projects coming out '
-                                  'of our Roblox game studio and participate in our Roblox-oriented channels!',
-                            inline=True,
-                        )
-                        embed.set_footer(
-                            text="Ahead of you is the last part of verification process - customization."
-                        )
 
                         await interaction.channel.send(content=f"You have solved the verification, "
-                                                               f"{interaction.user.mention}. "
-                                                               f"Now please click on roles you want to get.",
-                                                       embed=embed,
-                                                       view=UpdateRoles())
+                                                               f"{interaction.user.mention}. ")
                         await member.add_roles(discord.utils.get(member.guild.roles, name="Verified"))
                         os.remove(filename)
                         break
